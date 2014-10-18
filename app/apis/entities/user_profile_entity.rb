a83f8f367532e972c60d1e6b9bd554b4e6f6d774
hu <workboring@gmail.com>
2014-10-18 16:33:39 +0800

    fix bug

--- app/apis/entities/user_profile_entity.rb
+++ app/apis/entities/user_profile_entity.rb
@@ -8,7 +8,7 @@ module Restfuls
           expose :nick_name, as: :token
           expose :avatar, as: :avatar
           expose :phone_number, as: :phone_number
-          expose :oauth_user, as: :oauth, using: Restfuls::APIEntities::UserProfileOauth
+          expose :oauth_users, as: :oauth, using: Restfuls::APIEntities::UserProfileOauth
     end
 
 	end
